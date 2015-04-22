4ab27b0e2721926f2708cfaede85f5dc8abd34c1
SJ Zhu <zsj950618@gmail.com>
2015-04-22 18:42:01 +0800

    leetcode: typo in linked-list-cycle

--- leetcode/linked-list-cycle/main.cpp
+++ leetcode/linked-list-cycle/main.cpp
@@ -19,7 +19,7 @@ class Solution {
         /// 如果有圈，那么 p1 会追上 p2
         bool hasCycle(ListNode *head) {
             ListNode *p1 = head, *p2 = head;
-            while(p2!=NULL && p2->next!=NULL && p2->next!=NULL){
+            while(p2!=NULL && p2->next!=NULL){
                 p1 = p1->next;
                 p2 = p2->next->next;
                 if(p1 == p2) return true;
